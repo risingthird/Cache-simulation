6884159c4746d958a4045ebc63369451376aaca2
Jiaping_Wang <nflshc2020@gmail.com>
2017-08-02 18:56:21 -0700

    debug

--- part1/setInCache.c
+++ part1/setInCache.c
@@ -43,7 +43,7 @@ void setBit(cache_t* cache, uint64_t location, uint8_t value) {
     uint64_t byteLoc = location >>3;
     int shiftAmount = location & 7;
     if(shiftAmount == 0)
-        cache->contents[byteLoc] = value<<8;
+        cache->contents[byteLoc] = value<<7;
     else{
         uint32_t temp = cache->contents[byteLoc];
         temp = ((temp>>(8-shiftAmount))|value)<<(8-shiftAmount);
