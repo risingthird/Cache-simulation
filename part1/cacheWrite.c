afa0f427adfc3944d3781b764be2dcb146213ccd
Jiaping_Wang <nflshc2020@gmail.com>
2017-08-07 09:59:46 -0700

    read

--- part1/cacheWrite.c
+++ part1/cacheWrite.c
@@ -49,7 +49,7 @@ void writeToCache(cache_t* cache, uint32_t address, uint8_t* data, uint32_t data
         uint32_t oldTag = extractTag(cache,info->blockNumber);
         //data1[offset] = data;
         
-        writeDataToCache(cache, address-offset,data1,cache->blockDataSize,oldTag, info);
+        writeDataToCache(cache, address,data,dataSize,oldTag, info);
         free(info);
     }
 }
