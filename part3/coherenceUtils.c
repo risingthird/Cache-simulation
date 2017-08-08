babc2d988d6f80760eba5b87654f1a3f135aa928
Bowen Yang <gloriayangbowen@gmail.com>
2017-08-08 00:13:05 -0700

    3

--- part3/coherenceUtils.c
+++ part3/coherenceUtils.c
@@ -249,7 +249,7 @@ void updateState(cache_t* cache, uint32_t address, enum state otherState) {
 			setShared(cache,blockNumber,0);
 		}
 	}
-	long oldLRU = getLRU(cache,blocknumber);
+	long oldLRU = getLRU(cache,blockNumber);
 	uint32_t tag = getTag(cache,address);
 	uint32_t idx = getIndex(cache,address);
 	decrementLRU(cache, tag, idx, oldLRU);
